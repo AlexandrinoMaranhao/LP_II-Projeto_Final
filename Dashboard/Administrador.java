56a9477f855588a0770ee49f190489e31549d30d
Marcio "Alexandrino" Sá <78682655+AlexandrinoMaranhao@users.noreply.github.com>
2023-12-20 13:57:31 -0300

    Add files via upload

---
+++ Dashboard/Administrador.java
@@ -0,0 +1,21 @@
+package poo.dashboardsistema;
+
+public class Administrador {
+	private String loginAdmin; // default: root
+	private String senhaAdmin; // default: root
+
+	/*
+	 * A organização também sugere a criação de um módulo no qual os Administradores
+	 * possam emitir relatórios sobre a utilização do sistema: 
+	 * 1) Quantidade e percentual de atas emitidas por setor em relação ao total de atas; 
+	 * 2) Quantidade e percentual de atas emitidas por funcionário; 
+	 * 3) Tempo médio, máximo e mínimo de duração das reuniões.
+	 * 
+	 * A fim de gerar estatísticas, o administrador deve informar o período inicial
+	 * e final e qual meio de saída dos resultados (tela ou arquivo de texto).
+	 * 
+	 *	1 - 
+	 * 
+	 *	2 -
+	 */
+}
